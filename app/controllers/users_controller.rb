55006907e9c225aaf3b4d7b6d4a28261506d5db8
Dean Mandile <Deanskee@Deans-MacBook-Pro.local>
2014-09-08 16:14:24 -0700

    hopefully fixed the create/signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
       respond_to do |format|
       flash[:success] = "Thank you for signing up!"
       UserMailer.welcome_email(@user).deliver
-      session_create # This method is inhereited from the applicaiton_controller
+      session[:remember_token] = @user.id
+      # session_create # This method is inhereited from the applicaiton_controller
       format.html  {redirect_to user_path(current_user), notice: "Welcome new user."}
       format.json { render json: @user, status: :created}
    end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
     def current_user
-        @current_user ||= session[:remember_token] && User.find(session[:remember_token])
+        @current_user ||= session[:remember_token] && User.where(id: session[:remember_token]).first
     end
 
     def signed_in?
